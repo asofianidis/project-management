2334b8882753482ed503d4137551054c350c79b5
Alexander Sofianidis <ansofianidis@outlook.com>
2024-02-15 21:02:39 -0600

    Created DbContext, added connection string, and created basic user model

---
+++ backend/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace backend.Data;
+
+public class AppDbContext : DbContext
+{
+    private readonly IConfiguration _configuration;
+
+    public AppDbContext(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseNpgsql(_configuration.GetConnectionString("default"));
+}
---
+++ backend/Models/User.cs
@@ -0,0 +1,13 @@
+﻿namespace backend.Models;
+
+public class User
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    public string Salt { get; set; }
+    public string Pass { get; set; }
+    public bool IsAdmin { get; set; }
+    public DateTime Created { get; set; }
+}
