494a90a4447e437a044f6894d62fc9d934d7fa9c
Alexander Sofianidis <ansofianidis@outlook.com>
2024-02-15 20:46:11 -0600

    initial backend commit

---
+++ backend/Program.cs
@@ -0,0 +1,35 @@
+namespace backend;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        builder.Services.AddAuthorization();
+        builder.Services.AddControllers();
+
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+        app.MapGet("/", context => context.Response.WriteAsync("Hello World"));
+        app.MapControllers();
+        
+        app.Run();
+    }
+}
